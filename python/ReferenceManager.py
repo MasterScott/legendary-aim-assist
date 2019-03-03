450e5fe97ad8d4724ddbc4a6fe21e76d415d529b
Eric Maynard <eric.w.maynard@gmail.com>
2019-03-03 03:14:52 -0500

    started poc work

---
+++ python/ReferenceManager.py
@@ -0,0 +1,41 @@
+import enum
+import numpy as np
+
+def scope_string(scope):
+    return scope[len('Scope.'):]
+
+class Scope(enum.Enum):
+    x2 = 1
+
+
+class AreaOfInterest:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+
+class HsvBounds:
+    def __init__(self, lower, upper):
+        self.lower = lower
+        self.upper = upper
+
+# TODO
+def get_aoi(type):
+    if type == Scope.x2:
+        return AreaOfInterest(815, 480, 280, 120)
+    else:
+        raise Exception("Unknown scope!")
+
+def get_hsv(type):
+    if type == Scope.x2:
+        return HsvBounds(np.array([0, 150, 0]), np.array([50, 230, 160]))
+    else:
+        raise Exception("Unknown scope!")
+
+def get_aim(type):
+    if type == Scope.x2:
+        return [145, 60]
+    else:
+        raise Exception("Unknown scope!")
---
+++ python/main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+from ReferenceManager import Scope
+import ReferenceManager
+
+# read in the image:
+img = cv2.imread('../examples/beast-mode/x2/0.png')
+
+# transform the image:
+
+# crop to AOI:
+aoi = ReferenceManager.get_aoi(Scope.x2)
+img = img[aoi.y:aoi.y+aoi.h, aoi.x:aoi.x+aoi.w]
+
+# filter for color:
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(img, ReferenceManager.get_hsv(Scope.x2).lower, ReferenceManager.get_hsv(Scope.x2).upper)
+img = cv2.bitwise_and(img, img, mask=mask)
+
+# erode and dilate:
+img = cv2.erode(img, np.ones((2, 1), np.uint8), iterations=1)
+img = cv2.dilate(img, np.ones((2, 2), np.uint8), iterations=5)
+
+# identify objects:
+img[img != 0] = 255
+cv2.floodFill(img, None, (0, 0), 255)
+# _, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)
+# img = cv2.bitwise_not(img)
+# h, w = img.shape[:2]
+# mask = np.zeros((h + 2, w + 2), np.uint8)
+# cv2.floodFill(img, mask, (0, 0), 255)
+
+
+# show the result:
+cv2.imshow('test', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
